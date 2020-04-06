9a0693371b1b1ec19d5c55ef922943791c65df17
Alan Lam <s3436174@student.rmit.edu.au>
2020-04-06 13:45:05 +1000

    Uploading milestone#2 again

---
+++ Breadcrumb.cpp
@@ -0,0 +1,42 @@
+
+#include "Breadcrumb.h"
+
+Breadcrumb::Breadcrumb(int x, int y, bool stale):
+   x(x),
+   y(y),
+   stale(stale)
+{
+   // this->x = x;
+   // this->y = y;
+   // this->stale = stale;
+}
+
+Breadcrumb::Breadcrumb(Breadcrumb& other) :
+   x(other.x),
+   y(other.y),
+   stale(other.stale)
+{
+   // this->x = other.x;
+   // this->y = other.y;
+   // this->stale = other.stale;
+}
+
+Breadcrumb::~Breadcrumb() {
+   
+}
+
+int Breadcrumb::getX() {
+   return x;
+}
+
+int Breadcrumb::getY() {
+   return y;
+}
+
+bool Breadcrumb::isStale() {
+   return stale;
+}
+
+void Breadcrumb::setStale(bool stale) {
+   this->stale = stale;
+}
---
+++ Trail.cpp
@@ -0,0 +1,53 @@
+
+#include "Trail.h"
+
+
+Trail::Trail() {
+   this->length = 0;
+   for(int i = 0; i < TRAIL_ARRAY_MAX_SIZE; ++i) {
+      breadcrumbs[i] = nullptr;
+   }
+}
+
+Trail::Trail(Trail& other) {
+   this->length = other.length;
+
+   for (int i = 0; i < TRAIL_ARRAY_MAX_SIZE; ++i) {
+      this->breadcrumbs[i] = nullptr;
+      if(other.breadcrumbs[i] != nullptr){
+         this->breadcrumbs[i] = new Breadcrumb(*other.breadcrumbs[i]);
+      }
+   }
+
+}
+
+Trail::~Trail() {
+   for(int i = 0; i < length; ++i) {
+      if(breadcrumbs[i] != nullptr){
+         delete breadcrumbs[i];
+         breadcrumbs[i] = nullptr;
+      }
+   }
+}
+
+int Trail::size() {
+   return length;
+}
+
+Breadcrumb* Trail::getPtr(int i) {
+   return breadcrumbs[i];
+}
+
+void Trail::addCopy(Breadcrumb* t) {
+   breadcrumbs[length] = t;
+   length++;
+}
+
+bool Trail::contains(int x, int y) {
+   for(int i = 0; i < length; i++) {
+      if(breadcrumbs[i]->getX() == x && breadcrumbs[i]->getY() == y) {
+         return true;
+      }
+   }
+   return false;
+}
